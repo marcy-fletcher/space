2bc4907876d7074f22ed22e800824345d48c7e8a
Marcy Fletcher <fletchermarcy0@gmail.com>
2026-01-12 19:05:26 +0300

    Try?

--- src/pages/Maintenance.tsx
+++ src/pages/Maintenance.tsx
@@ -6,7 +6,7 @@ const Maintenance = () => {
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-screen bg-linear-to-br from-primary-50 via-shade-50 to-primary-200 dark:from-mono-900 dark:via-mono-800 dark:to-mono-900">
-            <img className="grow max-h-125 max-w-125 object-contain select-none" src="/maintenance.webp" alt="Maintenance"/>
+            <img className="grow max-h-125 max-w-125 object-contain select-none" src="maintenance.webp" alt="Maintenance"/>
         </div>
     );
 };
