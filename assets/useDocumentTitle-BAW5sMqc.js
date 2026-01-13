a552aa9f1351c5cbed7f5df4dc37582530fb19f9
marcy-fletcher <marcy-fletcher@users.noreply.github.com>
2026-01-13 15:39:30 +0000

    deploy: f79f7ff066aaa11713a2188efcf0967737235d71

---
+++ assets/useDocumentTitle-BAW5sMqc.js
@@ -0,0 +1,1 @@
+import{a as r}from"./vendor-font-awesome-fb8PRCmj.js";import{A as u}from"./index-CCx6gsre.js";function a(t,e=u.appName){r.useEffect(()=>{if(!t)return;const o=document.title;return document.title=`${t} - ${e}`,()=>{document.title=o}},[t,e])}export{a as u};
