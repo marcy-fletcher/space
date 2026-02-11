6f9e812f0968423b6632fcac9326565fedeec55f
Marcy Fletcher <fletchermarcy0@gmail.com>
2026-02-11 19:22:53 +0500

    Temporary removed geoblock

--- src/common/hooks/useGeoBlock.ts
+++ src/common/hooks/useGeoBlock.ts
@@ -1,35 +1,5 @@
-import {useQuery} from '@tanstack/react-query';
-import {AppMeta} from "../../appMeta";
-
-
-interface IpResponse {
-    country_code: string
-}
-
-const fetchGeoLocation = async () => {
-    const response = await fetch('https://ipwhois.app/json/');
-    if (!response.ok) {
-        throw new Error('Failed to fetch geolocation data');
-    }
-    return response.json();
-};
-
 const useGeoBlock = () => {
-    const {data, error, isLoading} = useQuery<IpResponse, Error>({
-            queryKey: ['geo-check'],
-            queryFn: () => fetchGeoLocation(),
-            staleTime: 5 * 60 * 1000,
-            refetchInterval: 5 * 60 * 1000,
-            enabled: AppMeta.enableGeoCheck,
-            retry: 1
-        }
-    );
-
-    const blockedCountries: string[] = AppMeta.blockedCountries;
-    const isChecked = !AppMeta.enableGeoCheck || (!!data || error);
-    const isAvailable = !AppMeta.enableGeoCheck || (data && !blockedCountries.includes(data?.country_code?.toLowerCase()));
-
-    return {isChecked, isAvailable, isLoading, error};
+    return {isChecked:true, isAvailable:true, isLoading:false, error:undefined};
 };
 
 export default useGeoBlock;
