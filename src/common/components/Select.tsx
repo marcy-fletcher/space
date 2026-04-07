1ac01422d27f66d0d5246e82a65423951b8be089
Marcy Fletcher <fletchermarcy0@gmail.com>
2026-04-08 02:26:15 +0500

    Select updated

--- src/common/components/Select.tsx
+++ src/common/components/Select.tsx
@@ -13,7 +13,7 @@ interface SelectProps {
 
 const Select = ({label, value, options, className, onChange}: SelectProps) => {
     return (
-        <div className={cn("flex items-center gap-2", className)}>
+        <div className={cn("flex flex-col items-stretch gap-2 sm:flex-row sm:items-center", className)}>
             {label && <label htmlFor="sort-select"
                     className="text-sm font-medium text-mono-700 dark:text-mono-300 whitespace-nowrap select-none">
                 {label}
@@ -22,7 +22,7 @@ const Select = ({label, value, options, className, onChange}: SelectProps) => {
                 id="sort-select"
                 value={value}
                 onChange={(e) => onChange?.(e.target.value)}
-                className="grow px-3 py-2 border border-mono-300 dark:border-mono-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500 dark:bg-mono-700 dark:text-mono-50 text-sm"
+                className="w-full grow px-3 py-2 border border-mono-300 dark:border-mono-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500 dark:bg-mono-700 dark:text-mono-50 text-sm"
             >
                 {
                     options.map((option) => (
